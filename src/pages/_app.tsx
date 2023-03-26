80f1dff3821c832981d60546a42e8abdef838249
Christian Vazquez <christian.vazquez@ticc.com.mx>
2023-03-26 00:17:46 -0600

    first loading implemented

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,21 @@
 import { wrapper } from '@/features/store';
 import type { AppProps } from 'next/app';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
+    setLoading(true);
 		const mainScript = document.createElement("script");
 		mainScript.src = "js/main.js";
 		setTimeout(() => {
       document.body.appendChild(mainScript);
-    }, 500);
+    }, 1000);
+    setLoading(false);
 	});
 
-  return <Component {...pageProps} />
+  return loading ? <>Loading...</> : <Component {...pageProps} />
 }
 
 export default wrapper.withRedux(App);
